13465791df2bb5a6cea47b31e7e605c6e6852ba5
bloodearnest <bloodearnest@gmail.com>
2026-02-04 12:07:43 +0000

    Add school_id column to teachers and researchers sql
    For teachers, this already exists, and is 100% populated, so is a simple
    change.
    
    For results, its a bit more complicated. Some attainments have classId,
    and classes always have a schoolId. However, some do not have associated
    classes, so we must fallback to to student.schoolId.
    
    I also validated that when present, classes.schoolId matches
    student.schoolId with the following:
    
    ```
    WITH attainmentsWithRowNum AS (
        SELECT
            attainmentKey,
            studentId,
            classId,
            assessmentId,
            attainmentDate,
            score,
            ROW_NUMBER() OVER (PARTITION BY attainmentKey ORDER BY classId ASC) AS rn
        FROM attainments
        WHERE len(score) <= 6
    ),
    dedupedAttainments AS (
        SELECT
            attainmentKey,
            studentId,
            classId,
            assessmentId,
            attainmentDate,
            score
        FROM attainmentsWithRowNum
        WHERE rn = 1
    )
    SELECT COUNT(*) AS school_id_mismatch
    FROM dedupedAttainments AS at
    LEFT JOIN classes AS c
        ON c.classKey = at.classId
    LEFT JOIN students AS s
        ON s.studentKey = at.studentId
    WHERE c.schoolId IS NOT NULL
    AND s.schoolId IS NOT NULL
    AND c.schoolId <> s.schoolId;
    
    school_id_mismatch
    ------------------
                     0
    ```

--- sql/teachers.sql
+++ sql/teachers.sql
@@ -5,9 +5,10 @@ Apart from specialism, all values for a given teacherKey are the same.
 
 SELECT
     teacherKey AS teacher_id,
+    schoolId as school_id,
     -- payscale is recorded as eg `L03 11.0`; we just want `L03`.
     MAX(LEFT(payscale, CHARINDEX(' ', payscale + ' ') - 1)) AS payscale,
     STRING_AGG(specialism, '; ') WITHIN GROUP (ORDER BY specialism) AS specialism,
     MAX(DATEFROMPARTS(YEAR(dateStartedSchool), MONTH(dateStartedSchool), 1)) AS date_started_school
 FROM teachers
-GROUP BY teacherKey
+GROUP BY teacherKey, schoolId
